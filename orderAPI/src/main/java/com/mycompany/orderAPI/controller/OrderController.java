02038d3d41229ead3dfcdef6ff83338540cbb9fe
Chenjae <injea95@naver.com>
2021-11-24 14:29:00 +0900

    HYUN-127 <주문 목록 조회>

---
+++ orderAPI/src/main/java/com/mycompany/orderAPI/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.mycompany.orderAPI.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.orderAPI.service.OrderService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class OrderController{
+	@Resource
+	private OrderService orderService;
+	
+	@GetMapping("/list")
+	public Map<String, Object> list(@RequestBody String mid) {
+		log.info("실행");
+		log.info("memberId : " + mid);
+		
+		List<Order> list = orderService.getOrders(mid);
+		Map<String, Object> map = new HashMap<>();
+		map.put("orders", list);
+		
+		return map;
+	}
+}
