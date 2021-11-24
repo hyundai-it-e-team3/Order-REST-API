f1867742b27d27f7a9fa0362012c3eec09d59af5
Chenjae <injea95@naver.com>
2021-11-25 00:38:20 +0900

    HYUN-94 <장바구니 상품 조회/추가/수정/삭제>

--- orderAPI/src/main/java/com/mycompany/orderAPI/controller/CartController.java
+++ orderAPI/src/main/java/com/mycompany/orderAPI/controller/CartController.java
@@ -1,8 +1,49 @@
 package com.mycompany.orderAPI.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.orderAPI.dto.member.Member;
+import com.mycompany.orderAPI.dto.order.Cart;
+import com.mycompany.orderAPI.service.CartService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/order")
+@Slf4j
 public class CartController {
+	@Resource CartService cartService;
+	
+	@GetMapping
+	public List<Cart> getCarts(@RequestBody Member member) {
+		return cartService.getCarts(member);
+	}
+	
+	@PostMapping
+	public void insert(@RequestBody Cart cart) {
+		cartService.insert(cart);
+	}
+	
+	@PatchMapping
+	public void update(@RequestBody Cart cart) {
+		cartService.update(cart);
+	}
+	
+	@DeleteMapping
+	public void delete(@RequestBody Cart cart) {
+		cartService.delete(cart);
+	}
+	
+	
 
 }
--- orderAPI/src/main/java/com/mycompany/orderAPI/service/CartService.java
+++ orderAPI/src/main/java/com/mycompany/orderAPI/service/CartService.java
@@ -1,8 +1,55 @@
 package com.mycompany.orderAPI.service;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+
+import com.mycompany.orderAPI.dao.CartDao;
+import com.mycompany.orderAPI.dto.member.Member;
+import com.mycompany.orderAPI.dto.order.Cart;
 
 @Service
 public class CartService {
-
+	@Resource CartDao cartDao;
+	
+	public enum CartResult {
+		SUCCESS,
+		SUCCESS_DUPLICATE,
+		FAIL_DUPLICATE
+	}
+	
+	public List<Cart> getCarts(Member member) {
+		return cartDao.selectByMid(member.getMemberId());
+	}
+	
+	public CartResult insert(Cart cart) {
+		Cart dbCart = cartDao.selectCart(cart);
+		if(dbCart == null) {
+			cartDao.insert(cart);
+			return CartResult.SUCCESS;
+		} else {
+			cart.setAmount(cart.getAmount()+dbCart.getAmount());
+			cartDao.update(cart);
+			return CartResult.SUCCESS_DUPLICATE;
+		}
+	}
+	
+	public void delete(Cart cart) {
+		cartDao.delete(cart);
+	}
+	
+	public CartResult update(Cart cart) {
+		Cart dbCart = cartDao.selectCart(cart);
+		if(dbCart == null) {
+			cartDao.update(cart);
+			return CartResult.SUCCESS;
+		} else {
+			dbCart.setAmount(cart.getAmount()+dbCart.getAmount());
+			cartDao.update(dbCart);
+			cartDao.delete(cart);
+			return CartResult.SUCCESS_DUPLICATE;
+		}
+	}
 }
