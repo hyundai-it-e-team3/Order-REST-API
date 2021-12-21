d9911bda7f0c5c117327cf82a9425b6194231b54
Chenjae <injea95@naver.com>
2021-12-21 11:13:37 +0900

    HYUN-94 <장바구니 수량 수정>

--- orderAPI/src/main/java/com/mycompany/orderAPI/service/CartService.java
+++ orderAPI/src/main/java/com/mycompany/orderAPI/service/CartService.java
@@ -1,6 +1,8 @@
 package com.mycompany.orderAPI.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -9,9 +11,14 @@ import org.springframework.stereotype.Service;
 import com.mycompany.orderAPI.dao.orderDB.CartDao;
 import com.mycompany.orderAPI.dto.member.Member;
 import com.mycompany.orderAPI.dto.order.Cart;
+import com.mycompany.orderAPI.dto.product.StockDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class CartService {
+	@Resource StockService stockService;
 	@Resource CartDao cartDao;
 	
 	public enum CartResult {
@@ -53,11 +60,39 @@ public class CartService {
 		}
 	}
 	
-	public void updateAmount(String cartId, int amount) {
+	public Map<String, Object> updateAmount(String cartId, int amount) {
+		log.info("실행");
+		Cart cartInfo = getCart(cartId);
 		Cart cart = new Cart();
 		cart.setCartId(cartId);
-		cart.setAmount(amount);
+		
+		Map<String,Object> result = new HashMap<String,Object>();
+		
+		//재고량 확인
+		StockDTO stock = new StockDTO();
+		stock.setProductDetailId(cartInfo.getProductDetailId());
+		stock.setPsize(cartInfo.getPsize());
+		stock = stockService.getStock(stock);
+		log.info("stock : " + stock);
+		if(stock.getAmount() == 0) {
+			//매진
+			cart.setAmount(0);
+			result.put("result", "soldout");
+		}
+		if(stock.getAmount() < amount) {
+			//수량 부족
+			log.info("수량부족");
+			cart.setAmount(stock.getAmount());
+			result.put("result", "lowStock");
+		} else {
+			//수량 충분
+			cart.setAmount(amount);
+			result.put("result", "success");
+		}
+		log.info("cart : " + cart);
 		cartDao.updateAmount(cart);
+		result.put("amount", cart.getAmount());
+		return result;
 	}
 	
 	
