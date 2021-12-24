7c470d095e614345da3915d516c957e75e591007
Chenjae <injea95@naver.com>
2021-12-24 15:16:03 +0900

    SETTING <Spring Gateway 설정>

--- orderAPI/src/main/java/com/mycompany/orderAPI/OrderApiApplication.java
+++ orderAPI/src/main/java/com/mycompany/orderAPI/OrderApiApplication.java
@@ -3,7 +3,10 @@ package com.mycompany.orderAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 @EnableDiscoveryClient
 public class OrderApiApplication {
